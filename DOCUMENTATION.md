2945547ca21a69ac58267a51478b5b6c66ce149e
Ionică Bizău <bizauionica@gmail.com>
2016-07-01 08:28:56 +0300

    Working version

---
+++ DOCUMENTATION.md
@@ -0,0 +1,14 @@
+## Documentation
+
+You can see below the API reference of this module.
+
+### `valueBreakpoints(a, b)`
+Run specific stuff when value reaches a specific range.
+
+#### Params
+- **Number** `a`: Param descrpition.
+- **Number** `b`: Param descrpition.
+
+#### Return
+- **Number** Return description.
+
--- example/index.js
+++ example/index.js
@@ -1,5 +1,69 @@
 "use strict";
 
-const valueBreakpoints = require("../lib");
+const ValueBreakpoints = require("../lib");
 
-console.log(valueBreakpoints());
+let value = 0;
+
+setInterval(() => ++value, 500);
+
+let brks = new ValueBreakpoints([
+    {
+        min: 10
+      , max: 30
+      , start: () => {
+            console.log("Start");
+        }
+      , inside: () => {
+            console.log("Inside");
+        }
+      , end: () => {
+            console.log("End");
+        }
+    }
+  , {
+        min: 5
+      , max: 10
+      , end: () => {
+            console.log("Second End");
+        }
+    }
+]);
+
+brks.check(() => {
+    console.log(value);
+    return value;
+});
+// 0
+// 2
+// 4
+// 6
+// 7
+// 9
+// 11
+// Start
+// Inside
+// Second End
+// 13
+// Inside
+// 15
+// Inside
+// 17
+// Inside
+// 19
+// Inside
+// 21
+// Inside
+// 23
+// Inside
+// 25
+// Inside
+// 27
+// Inside
+// 29
+// Inside
+// 31
+// End
+// 33
+// 35
+// 37
+// ...
